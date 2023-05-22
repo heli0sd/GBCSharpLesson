05a8cbddc29753424c4705e849858bd2c7c10922
Danil <shumihind@gmail.com>
2023-05-22 09:36:57 +0600

    Solution Task 002

---
+++ Lesson1/Task002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Программа сравнения двух чисел");
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+
+if (num1 > num2)
+{
+     Console.WriteLine($"Число {num1} больше чем {num2}");
+}   
+else if (num1 < num2)
+{
+    Console.WriteLine($"Число {num1} меньше чем {num2}");
+}  
+else
+{
+    Console.WriteLine($"Число {num1} и число {num2} равны");
+}
+    
---
+++ Lesson1/Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Программа нахождения максимального числа из 3х введенных чисел");
+Console.Write("Введите первое целое число: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int num3 = Convert.ToInt32 (Console.ReadLine());
