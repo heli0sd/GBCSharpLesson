808eba8424df4f1dd3de14dc1a351fce5b241cdd
Danil <shumihind@gmail.com>
2023-05-22 10:15:14 +0600

    Solution Task 004

--- Lesson1/Task004/Program.cs
+++ Lesson1/Task004/Program.cs
@@ -9,3 +9,10 @@ Console.Write("Введите второе целое число: ");
 int num2 = Convert.ToInt32 (Console.ReadLine());
 Console.Write("Введите третье целое число: ");
 int num3 = Convert.ToInt32 (Console.ReadLine());
+
+if (num1 > num2 && num1 > num3)
+   Console.WriteLine($"Наибольшее из трех чисел {num1}");
+else if (num2 > num3)
+    Console.WriteLine($"Наибольшее из трех чисел {num2}");
+else 
+    Console.WriteLine($"Наибольшее из трех чисел {num3}");
