39956f52e40eeb995c1bfadfe958de47d71028b1
Danil <shumihind@gmail.com>
2023-05-23 10:44:29 +0600

    solution class work task 11

--- Lesson2/Demo1/Program.cs
+++ Lesson2/Demo1/Program.cs
@@ -3,3 +3,21 @@
 // * 78 -> 8
 // * 12 -> 2
 // * 85 -> 8
+
+Console.Clear();
+
+int num = new Random().Next(10, 100);
+
+System.Console.WriteLine($"Было сгенерировано случайное число: {num}");
+int x1 = num/10;
+int x2 = num%10;
+
+if (x1 > x2)
+    System.Console.WriteLine($"Больше цифра этого числа {x1}");
+else if (x1 < x2)
+    System.Console.WriteLine($"Больше цифра этого числа {x2}");
+else
+    System.Console.WriteLine("Обе цифры этого числа равны");
+
+for (int i=1;i<=10;i++)
+    System.Console.Write($"{i} ");
