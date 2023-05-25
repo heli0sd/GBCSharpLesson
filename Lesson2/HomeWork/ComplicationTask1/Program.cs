7527b3005eaa8545ad5068208e5c87f8cc223c23
Danil <shumihind@gmail.com>
2023-05-25 11:15:37 +0600

    Решена вторая сложная задача, второго семинара

--- Lesson2/HomeWork/ComplicationTask1/Program.cs
+++ Lesson2/HomeWork/ComplicationTask1/Program.cs
@@ -3,24 +3,24 @@
 // 1 -> нет
 // 567,123 -> 57,123
 
-System.Console.WriteLine("Введите любое число");
- Console.Write("Введите число N: ");
+Console.WriteLine("Введите любое число");
+Console.Write("Введите число N: ");
 double n = Convert.ToDouble (Console.ReadLine());
 
-double a = Math.Truncate(n); // int
+double a = Math.Truncate(n);
 double az = (int)Math.Log10(a) + 1;  
 double ax = Convert.ToDouble(az);
-double a1 = Math.Floor( n / (Math.Pow(10, ax-1)) ) %10; // 1st digit
-double a2 = Math.Floor( n / (Math.Pow(10, ax-2)) ) %10; // 2nd digit
+double a1 = Math.Floor( n / (Math.Pow(10, ax-1)) ) %10;
+double a2 = Math.Floor( n / (Math.Pow(10, ax-2)) ) %10;
 double tt2 = 0;
 
 if (ax>1) 
 {
     tt2 = n - a2 * Math.Pow(10, ax-2) - a1*9*Math.Pow(10, ax-2);
+    Console.WriteLine(Math.Round(tt2, 9));
 } 
 else 
 {
-    tt2 = Math.Truncate(n);
+    System.Console.WriteLine("Нет второй цифры");
 }
 
-System.Console.WriteLine(Math.Round(tt2, 9));
