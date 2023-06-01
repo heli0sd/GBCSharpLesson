26675ba33fc85b1a05ad04e6cddb9e6aad4628cf
Danil <shumihind@gmail.com>
2023-06-01 12:11:41 +0600

    Решена сложная задача 4го урока

--- Lesson4/HomeWork/Task026Hard/Program.cs
+++ Lesson4/HomeWork/Task026Hard/Program.cs
@@ -1,77 +1,14 @@
 ﻿// Задача 26 HARD Напишите программу, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе. Строки использовать нельзя
 // 456 -> 3
 // 0 -> 1
-// 89,126 -> 5
+//89,126 -> 5
 // 0,001->4
-using System;
+//Enumerable.Range(0,10).Select(v => number * Math.Pow(10, v)).Select((d,i) => new {d , i}).First(x => x.d == (long) x.d).i;
+Console.Write("Введите число: ");
+double number;
+bool isNumber = double.TryParse(Console.ReadLine(), out number);
+int BefCo = (int)Math.Log10(number) + 1;
+int AftCo = Enumerable.Range(0,17).Select(v => number * Math.Pow(10, v)).Select((d,i) => new {d , i}).First(x => x.d == (long) x.d).i;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.Write("Введите число: ");
-        double number;
-        bool isNumber = double.TryParse(Console.ReadLine(), out number);
-
-        if (isNumber)
-        {
-            int count = CountDigits(number);
-            Console.WriteLine("Количество цифр в числе: " + count);
-        }
-        else
-        {
-            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
-        }
-    }
-
-    static int CountDigits(double number)
-    {
-        long integerPart = (long)Math.Abs(number);
-        int integerPartCount = CountDigits(integerPart);
-
-        double fractionalPart = Math.Abs(number - Math.Truncate(number));
-        int fractionalPartCount = CountDigits(fractionalPart, true);
-
-        return integerPartCount + fractionalPartCount;
-    }
-
-    static int CountDigits(long number)
-    {
-        if (number == 0)
-        {
-            return 1;
-        }
-
-        int count = 0;
-        while (number != 0)
-        {
-            number /= 10;
-            count++;
-        }
-        return count;
-    }
-
-    static int CountDigits(double number, bool isFractionalPart)
-    {
-        if (number == 0)
-        {
-            return 1;
-        }
-
-        int count = 0;
-        double epsilon = 1e-10;
-
-        while (Math.Abs(number - Math.Round(number)) > epsilon)
-        {
-            number *= 10;
-            count++;
-        }
-
-        if (isFractionalPart)
-        {
-            count++; // Учитываем точку
-        }
-
-        return count;
-    }
-}
+int result = BefCo + AftCo;
+System.Console.WriteLine(result);
